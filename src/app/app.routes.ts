c497f18d5b7b02b42428bf97246ac3d06771c2ed
Devildcd <caneladavid19@gmail.com>
2026-05-20 11:37:59 -0400

    [FTR](app): set up global routing architecture and base navigation configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { AppRoutes } from './core/router/app-route-data.model';
+import { APP_PATHS } from './core/router/app-paths';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const APP_ROUTES: AppRoutes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: `${APP_PATHS.admin}/dashboard`,
+  },
+  {
+    path: APP_PATHS.auth,
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: APP_PATHS.admin,
+    canMatch: [authGuard],
+    loadChildren: () =>
+      import('./layout/admin-shell/admin.routes').then((m) => m.ADMIN_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: `${APP_PATHS.admin}/dashboard`,
+  },
+];
